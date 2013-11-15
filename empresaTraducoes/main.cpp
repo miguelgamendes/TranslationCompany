013ecd078fa302709a6968b29ed9f2a91d02627d
EZSPECIAL <ftlopes93@gmail.com>
2013-11-15 20:16:18 +0000

    Finally made this git stuff work, 30 minutes of my life wasted

--- empresaTraducoes/main.cpp
+++ empresaTraducoes/main.cpp
@@ -4,94 +4,238 @@
 #include "technicalTrans.h"
 #include "System.h"
 
-int main(){
-	System obama; //cause he's in charge of everything
-	bool userWantsOut = false;
-	char choice;
+int DisplayMenu();
+int AddMenu();
+int AddTranslatorMenu();
+int AddOrderMenu();
+System transforma;
 
-	std::cout << "###################################" << std::endl;
-	std::cout << "#                                 #" << std::endl;
-	std::cout << "#    EMPRESA DE TRADUCOES 9001    #" << std::endl;
-	std::cout << "#                                 #" << std::endl;
-	std::cout << "###################################" << std::endl;
+int main() {
+	while (true) {
+		std::string choicet;
+		char choice;
+		do {
+			system("CLS");
+			std::cout << "###################################" << std::endl;
+			std::cout << "#                                 #" << std::endl;
+			std::cout << "#      Traducoes Trans-forma      #" << std::endl;
+			std::cout << "#                                 #" << std::endl;
+			std::cout << "###################################" << std::endl;
 
-	while(!userWantsOut){
-		//obama.LoadFromFile();
+			std::cout << std::endl << std::endl << "a) Display..." << std::endl;
+			std::cout << "b) Adicionar..." << std::endl;
+			std::cout << "c) Editar..." << std::endl;
+			std::cout << "d) Remover..." << std::endl;
+			std::cout << "q) Sair" << std::endl << std::endl;
+			std::cout << "Input: ";
 
-		std::cout << "O que pretende gerir? (X para sair)" << std::endl;
-		std::cout << "\tA - Gerir tradutores" << std::endl;
-		std::cout << "\tB - Gerir encomendas" << std::endl << std::endl;
-		std::cout << ">";
-		std::cin >> choice;
+			std::cin >> choicet;
+		} while (choicet.length() != 1);
+		choice = tolower(choicet[0]);
+		switch (choice) {
+		case 'a':
+			DisplayMenu();
+			break;
+		case 'b':
+			AddMenu();
+			break;
+		case 'q':
+			return 0;
+			break;
+		default:
+			break;
+		}
+	}
+}
 
-		switch(choice){
-			bool back = false;
+int DisplayMenu() {
+	while (true) {
+		std::string choicet;
+		char choice;
+		do {
+			system("CLS");
+			std::cout << "###################################" << std::endl;
+			std::cout << "#                                 #" << std::endl;
+			std::cout << "#         Menu de Display         #" << std::endl;
+			std::cout << "#                                 #" << std::endl;
+			std::cout << "###################################" << std::endl;
 
-			case 'A':
-			case 'a':
-				while(!back){
-					std::cout << "O que pretende fazer? (X para regressar)" << std::endl;
-					std::cout << "\tA - Visualizar tradutores" << std::endl;
-					std::cout << "\tB - Adicionar tradutor" << std::endl;
-					std::cout << "\tC - Eliminar tradutor" << std::endl << std::endl;
-					std::cout << ">";
-					std::cin >> choice;
+			std::cout << std::endl << std::endl << "a) Display tradutores" << std::endl;
+			std::cout << "b) Display encomendas" << std::endl;
+			std::cout << "q) Voltar ao menu principal" << std::endl << std::endl;
+			std::cout << "Input: ";
 
-					switch(choice){
-						case 'A':
-						case 'a':
-							obama.DisplayTranslators();
-							break;
-						case 'B':
-						case 'b':
-							{
-								std::string name;
-								int years;
+			std::cin >> choicet;
+		} while (choicet.length() != 1);
+		choice = tolower(choicet[0]);
+		switch (choice) {
+		case 'a':
+			transforma.DisplayTranslators();
+			break;
+		case 'b':
+			transforma.DisplayOrders();
+			break;
+		case 'q':
+			return 0;
+			break;
+		default:
+			break;
+		}
+	}
+}
 
-								std::cout << "Que tipo de tradutor deseja adicionar?";
-								std::cout << "\t1 - Técnico" << std::endl;
-								std::cout << "\t2 - Noticioso" << std::endl;
-								std::cout << "\t3 - Literario" << std::endl << std::endl;
-								std::cout << ">";
-								std::cin >> choice;
+int AddMenu() {
+	while (true) {
+		std::string choicet;
+		char choice;
+		do {
+			system("CLS");
+			std::cout << "###################################" << std::endl;
+			std::cout << "#                                 #" << std::endl;
+			std::cout << "#         Menu de Adicao          #" << std::endl;
+			std::cout << "#                                 #" << std::endl;
+			std::cout << "###################################" << std::endl;
 
-								switch(choice)
-							}
-							break;
-						case 'C':
-						case 'c':
-							{
-								std::string name;
-								system("CLS");
-								std::cout << "Especifique o nome do tradutor que deseja eliminar:" << std::endl;
-								std::cout << ">";
-								std::cin >> name;
-								obama.EliminateTranslator(name);
-							}
-							break;
-						case 'X':
-						case 'x':
-							back = true;
-						default:
-							break;
-					}
-				}
-				break;
-			case 'B':
-			case 'b':
-				//order ops
-				break;
-			case 'X':
-			case 'x':
-				userWantsOut = true;
-				break;
-			default:
-				break;
+			std::cout << std::endl << std::endl << "a) Adicionar tradutores" << std::endl;
+			std::cout << "b) Adicionar encomendas" << std::endl;
+			std::cout << "q) Voltar ao menu principal" << std::endl << std::endl;
+			std::cout << "Input: ";
+
+			std::cin >> choicet;
+		} while (choicet.length() != 1);
+		choice = tolower(choicet[0]);
+		switch (choice) {
+		case 'a':
+			AddTranslatorMenu();
+			break;
+		case 'b':
+			AddOrderMenu();
+			break;
+		case 'q':
+			return 0;
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+int AddTranslatorMenu() {
+	while (true) {
+		std::string choicet;
+		char choice;
+		do {
+			system("CLS");
+			std::cout << "###################################" << std::endl;
+			std::cout << "#                                 #" << std::endl;
+			std::cout << "#        Adicionar tradutor       #" << std::endl;
+			std::cout << "#                                 #" << std::endl;
+			std::cout << "###################################" << std::endl;
+
+			std::cout << std::endl << std::endl << "a) Tradutor literario" << std::endl;
+			std::cout << "b) Tradutor tecnico" << std::endl;
+			std::cout << "c) Tradutor noticioso" << std::endl;
+			std::cout << "q) Voltar ao menu de adicao" << std::endl << std::endl;
+			std::cout << "Input: ";
+
+			std::cin >> choicet;
+		} while (choicet.length() != 1);
+		choice = tolower(choicet[0]);
+		int clear = 1;
+		int years;
+		std::string name, type;
+		switch (choice) {
+		case 'a':
+			std::cout << "Anos de experiencia: ";
+			std::cin >> years;
+			std::cout << "Nome: ";
+			std::cin >> name;
+			do {
+				std::cout << "Tipo (\"prosa\" ou \"poesia\"): ";
+				std::cin >> type;
+				if ((type == "poesia") || (type == "prosa"))
+					clear = 0;
+			} while (clear != 0);
+			transforma.AddTranslator(years, name, type, "lit");
+			break;
+		case 'b':
+			std::cout << "Anos de experiencia: ";
+			std::cin >> years;
+			std::cout << "Nome: ";
+			std::cin >> name;
+			std::cout << "Dominio: ";
+			std::cin >> type;
+			transforma.AddTranslator(years, name, type, "tech");
+			break;
+		case 'c':
+			std::cout << "Anos de experiencia: ";
+			std::cin >> years;
+			std::cout << "Nome: ";
+			std::cin >> name;
+			do {
+				std::cout << "Tipo (\"diario\" ou \"semanario\"): ";
+				std::cin >> type;
+				if ((type == "diario") || (type == "semanario"))
+					clear = 0;
+			} while (clear != 0);
+			transforma.AddTranslator(years, name, type, "news");
+			break;
+		case 'q':
+			return 0;
+			break;
+		default:
+			break;
 		}
 
-		//obama.SaveToFile();
-		system("CLS");
 	}
+}
 
-	return 0;
+int AddOrderMenu() {
+	while (true) {
+		std::string choicet;
+		char choice;
+		do {
+			system("CLS");
+			std::cout << "###################################" << std::endl;
+			std::cout << "#                                 #" << std::endl;
+			std::cout << "#       Adicionar encomenda       #" << std::endl;
+			std::cout << "#                                 #" << std::endl;
+			std::cout << "###################################" << std::endl;
+
+			std::cout << std::endl << std::endl << "a) Adicionar encomenda de texto em prosa" << std::endl;
+			std::cout << "b) Adicionar encomenda de texto poetico" << std::endl;
+			std::cout << "c) Adicionar encomenda de texto tecnico" << std::endl;
+			std::cout << "d) Adicionar encomenda de texto noticioso" << std::endl;
+			std::cout << "q) Voltar ao menu principal" << std::endl << std::endl;
+			std::cout << "Input: ";
+
+			std::cin >> choicet;
+		} while (choicet.length() != 1);
+		choice = tolower(choicet[0]);
+		int clear = 1; //ASDASSDASD
+		std::string id, originalLanguage, finalLanguage, author, subject, spec;
+		int max, nWords, blocks;
+		switch (choice) {
+		case 'a':
+			std::cout << "Linguagem do texto: ";
+			std::cin >> originalLanguage;
+			std::cout << "Linguage destino: ";
+			std::cin >> finalLanguage;
+			std::cout << "Titulo do livro: ";
+			std::cin >> subject;
+			std::cout << "Autor: ";
+			std::cin >> author;
+			std::cout << "Numero de palavras: ";
+			std::cin >> nWords;
+			std::cout << "Prazo da encomenda (min): ";
+			std::cin >> max;
+			transforma.AddOrder("L01", originalLanguage, finalLanguage, max, nWords, author, subject, 0, "prose");
+			break;
+		case 'q':
+			return 0;
+			break;
+		default:
+			break;
+		}
+	}
 }
